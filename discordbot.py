3a2fef87dc0078a228c2589d588a3fbf78d37104
yoppyi0623 <59519214+yoppyi0623@users.noreply.github.com>
2020-12-13 16:18:55 +0900

    reset

--- discordbot.py
+++ discordbot.py
@@ -16,10 +16,5 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-    
-
-@bot.command()
-async def po(ctx):
-    await ctx.send('嘘つくのやめてもらっていいですか')   
 
     bot.run(token)
